bc315946b3fa1fba4a74446deadc68881b2a4d4b
jdsteadycode <desaijeet11@gmail.com>
2025-07-26 15:06:20 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function FeedbackForm() {
+  // initial values (states)
+  let [isSent, setIsSent] = useState(false);
+  let [message, setMessage] = useState("");
+
+  // when message is submitted or review is submitted!
+  if (isSent) {
+    // update the DOM
+    return <h1>Thankyou for your feedback</h1>;
+  }
+
+  // otherwise, initially display the new Form
+  else {
+    return (
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          // alert(`Sending: "${message}"`);  // ignore the toast*
+
+          // update the state
+          setIsSent((isSent = !isSent));
+        }}
+      >
+        <textarea
+          placeholder="Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <br />
+        <button type="submit">Send</button>
+      </form>
+    );
+  }
+}
